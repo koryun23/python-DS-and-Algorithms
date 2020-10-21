79ee4b949cfd510e6d13d1e8481621bf9cbcc45b
koryun23 <koryun.grigoryan.5@gmail.com>
2020-10-21 17:51:25 +0400

    fibonacci's N-th number

--- fibonacci.py
+++ fibonacci.py
@@ -14,13 +14,13 @@ def fibonacci_iteratively(n):
     a = 0
     b = 1
     c = 1
-    for i in range(1, n-1,1):
+    for i in range(1, n,1):
         a = b
         b = c
         c = a+b
     return b
 
-print(fibonacci_recursively(8))
-print(fibonacci_recursively(2))
-print(fibonacci_recursively(3))
-print(fibonacci_recursively(11))
+print(fibonacci_iteratively(8))
+print(fibonacci_iteratively(2))
+print(fibonacci_iteratively(3))
+print(fibonacci_iteratively(11))
