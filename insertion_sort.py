fe2b51f2061f2816a4f0aaa5c17ef232c8057b97
koryun23 <koryun.grigoryan.5@gmail.com>
2020-10-28 18:14:45 +0400

    Create insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,11 @@
+def insertion_sort(arr):#[6,5,3,1,8,7,2,4]
+    for i in range(1,len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1] = key
+    return arr
+print(insertion_sort([6,5,3,1,8,7,2,4]))
+
