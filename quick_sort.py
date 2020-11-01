228ce4909ae395dde2b83109a6fad1b5da51c543
koryun23 <koryun.grigoryan.5@gmail.com>
2020-11-01 14:20:07 +0400

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,3 +1,29 @@
+numbers = [6,5,3,1,8,7,2,4]
+def get_pivot(arr, low, high): 
+    pivot_index = high
+    i = low
+    while i < pivot_index:
+        if arr[pivot_index] > arr[i]: 
+            i += 1
+        else:
+            arr[pivot_index], arr[pivot_index-1] = arr[pivot_index-1], arr[pivot_index]
+            arr[pivot_index], arr[i] = arr[i], arr[pivot_index]
+            pivot_index -= 1
+
+    return pivot_index
+def quick_sort(array, low, high):
+    if len(array) == 1:
+        return array
+    if low < high:
+        pi = get_pivot(array, low, high)
+        get_pivot(array, low, pi-1)
+        get_pivot(array, pi+1, high)
+        
+
+for i in range(len(numbers)):
+    print(numbers[i])
 
 
-print(quick_sort([3,7,8,5,2,1,9,5,4]), 0, 8)
+
+print(quick_sort(numbers), 0, len(numbers) - 1)
+
