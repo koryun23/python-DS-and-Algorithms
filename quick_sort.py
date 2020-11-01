ff17d258af1a8ec7adc76d311f76a361ff06224e
koryun23 <koryun.grigoryan.5@gmail.com>
2020-11-01 15:45:57 +0400

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,29 +1,20 @@
 numbers = [6,5,3,1,8,7,2,4]
 def get_pivot(arr, low, high): 
-    pivot_index = high
-    i = low
-    while i < pivot_index:
-        if arr[pivot_index] > arr[i]: 
+    i = low-1
+    pivot = arr[high]
+    for j in range(low, high):
+        if arr[j] <= pivot:
             i += 1
-        else:
-            arr[pivot_index], arr[pivot_index-1] = arr[pivot_index-1], arr[pivot_index]
-            arr[pivot_index], arr[i] = arr[i], arr[pivot_index]
-            pivot_index -= 1
-
-    return pivot_index
+            arr[i],arr[j] = arr[j],arr[i]
+    arr[i+1],arr[high] = arr[high], arr[i+1]
+    return i+1
 def quick_sort(array, low, high):
-    if len(array) == 1:
-        return array
     if low < high:
         pi = get_pivot(array, low, high)
-        get_pivot(array, low, pi-1)
-        get_pivot(array, pi+1, high)
+        quick_sort(array, low, pi-1)
+        quick_sort(array, pi+1, high)
+    return array
         
-
-for i in range(len(numbers)):
-    print(numbers[i])
-
-
-
-print(quick_sort(numbers), 0, len(numbers) - 1)
+print(get_pivot(numbers,0,len(numbers) - 1))
+print(quick_sort(numbers, 0, len(numbers) - 1))
 
