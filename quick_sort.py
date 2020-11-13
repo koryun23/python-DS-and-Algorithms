40b098ea5d25fafaab54ab0dbfb4f6138121c8dd
koryun23 <koryun.grigoryan.5@gmail.com>
2020-11-13 13:27:12 +0400

    changing the pivot index variable name

--- quick_sort.py
+++ quick_sort.py
@@ -9,9 +9,9 @@ def get_pivot(arr, low, high):
     return i+1
 def quick_sort(arr, low, high):
     if low < high:
-        pi = get_pivot(arr, low, high)
-        quick_sort(arr, low, pi-1)
-        quick_sort(arr, pi+1, high)
+        pivot_index = get_pivot(arr, low, high)
+        quick_sort(arr, low, pivot_index-1)
+        quick_sort(arr, pivot_index+1, high)
     return arr
 
 numbers = [6,5,3,1,8,7,2,4]    
