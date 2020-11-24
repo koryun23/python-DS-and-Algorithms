0fbdc4c7f9a90b6fd19d5966b82fdd3c85c52495
koryun23 <koryun.grigoryan.5@gmail.com>
2020-11-24 11:46:34 +0400

    Revert "."
    This reverts commit 82b7335a115da72561d1becc7313f8421c2db438.

--- queue_test.py
+++ queue_test.py
@@ -1,2 +1,15 @@
+from queue_impl import Line
+from linkedlist import LinkedList
+import unittest
+
+class Test(unittest.TestCase):
+    def enqueue_test(self):
+        ll = LinkedList()
+        line = Line()
+        self.assertTrue(ll.append(1) == line.enqueue(1))
+    def dequeue_test(self):
+        ll = LinkedList()
+        line = Line()
+        self.assertTrue(ll.remove(0) == line.dequeue())
 
         
