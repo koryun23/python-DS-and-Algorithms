9bd192c2a4a5411e7a22f86c01f1a94e73f5af67
koryun23 <koryun.grigoryan.5@gmail.com>
2020-11-24 11:53:30 +0400

    Update search.py

--- search.py
+++
@@ -1,9 +0,0 @@
-from bst import BinarySearchTree
-
-def search(arr,value):
-    bst = BinarySearchTree
-    for i in range(arr):
-        bst.insert(arr[i])
-    return bst.lookup(value)
-
-print(search([1,4,6,3,8]))
